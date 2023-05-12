2d90bf64143858dd950638a5beb3e8cfe6e1d9ae
Anrsgrl <esgerlianar003@gmail.com>
2023-05-12 16:08:04 +0400

    Header done

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,7 +3,23 @@ import "./Header.scss";
 
 const Header = () => {
     return (
-        <header>Header</header>
+        <header className='py-3'>
+            <div className="container">
+                <div className="header-content">
+                    <h2>Azleppet</h2>
+                    <ul>
+                        <li><button className='clean-button'>Home</button></li>
+                        <li><button className='clean-button'>About</button></li>
+                        <li><button className='clean-button'>Contact</button></li>
+                    </ul>
+                    <select name="sort-list" id="sort-list">
+                        <option value="en">EN</option>
+                        <option value="aze">AZE</option>
+                        <option value="ru">RU</option>
+                    </select>
+                </div>
+            </div>
+        </header>
     )
 }
 
