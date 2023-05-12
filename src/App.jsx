53663bdb3c56e8d6c96ef4bb6e84b19e15e202bb
Anrsgrl <esgerlianar003@gmail.com>
2023-05-12 15:58:38 +0400

    structure prepared

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,8 @@ import Home from "./pages/Home/Home";
 import About from "./pages/About/About";
 import Contact from "./pages/Contact/Contact";
 import Header from './components/Header/Header';
-
+import "./App.scss";
+import 'bootstrap/dist/css/bootstrap.css';
 function App() {
   return (
     <>
