da101af6dd41e22e4c0efc64fee21fec2ca6a365
Anrsgrl <esgerlianar003@gmail.com>
2023-05-12 16:23:07 +0400

    Hero done

---
+++ src/pages/Home/components/Hero.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import "./Hero.scss";
+
+const Hero = () => {
+    return (
+        <div className="hero">
+            <div className="hero-content">
+                <h1>Wood Pellet</h1>
+                <p>We deliver pellets all over the country by trucks, wagons and containers</p>
+            </div>
+        </div>
+    )
+}
+
+export default Hero
