a85094441275175da517683f8eb1028b2a9d0349
Anrsgrl <esgerlianar003@gmail.com>
2023-05-13 12:50:26 +0400

    footer done

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
 import "./Footer.scss";
+import { BiPhoneCall } from "react-icons/bi";
 
 const Footer = () => {
     return (
-        <footer>footer</footer>
+        <footer>
+            <div className="container">
+                <div className="footer-content">
+                    <span>Copyright 2023 © All Right Reserved</span>
+                    <div className="contact-info">
+                        <BiPhoneCall />
+                        <a href="tel:+9943003030">+994 300 30 30</a>
+                    </div>
+                </div>
+            </div>
+        </footer>
     )
 }
 
