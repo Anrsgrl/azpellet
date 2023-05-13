c83deb7d044d22c6b1c1e8b990eb0542030cf6fb
Anrsgrl <esgerlianar003@gmail.com>
2023-05-13 12:34:16 +0400

    AboutInfo done

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -3,6 +3,7 @@ import AboutWhat from './Components/AboutWhat/AboutWhat';
 import AboutStages from './Components/AboutStages/AboutStages';
 import AboutStagesMobile from './Components/AboutStages/AboutStagesMobile';
 import AboutServices from './Components/AboutServices/AboutServices';
+import AboutInfo from './Components/AboutInfo/AboutInfo';
 
 const About = () => {
     return (
@@ -11,6 +12,7 @@ const About = () => {
             <AboutStages />
             <AboutStagesMobile />
             <AboutServices />
+            <AboutInfo />
         </div>
     )
 }
