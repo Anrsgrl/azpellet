6a9d9c9ce33ff426806cc559430e6465cf620485
Anrsgrl <esgerlianar003@gmail.com>
2023-05-27 19:23:29 +0400

    Footer number changed with mail

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import "./Footer.scss";
-import { BiPhoneCall } from "react-icons/bi";
+import { FiMail } from "react-icons/fi";
 import { useTranslation } from 'react-i18next';
 
 const Footer = () => {
@@ -11,8 +11,8 @@ const Footer = () => {
                 <div className="footer-content">
                     <span>{t('Copyright')}</span>
                     <div className="contact-info">
-                        <BiPhoneCall />
-                        <a href="tel:+9943003030">+994 300 30 30</a>
+                        <FiMail />
+                        <a href="mailto:info@azpellet.com">info@azpellet.com</a>
                     </div>
                 </div>
             </div>
