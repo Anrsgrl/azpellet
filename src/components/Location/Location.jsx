2dafe032d73911fa0175aefa705f5ad02ec20f26
Anrsgrl <esgerlianar003@gmail.com>
2023-05-31 11:46:38 +0400

    Location height fixed

--- src/components/Location/Location.jsx
+++ src/components/Location/Location.jsx
@@ -5,8 +5,8 @@ const Location = () => {
         <div data-aos="zoom-in" className="location">
             <iframe
                 title="map"
-                src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3034.0598800124453!2d49.8260738!3d40.4960589!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40308ec2727af697%3A0xc466712141b59f59!2sMehdiabad!5e0!3m2!1sen!2saz!4v1685126730697!5m2!1sen!2saz"
-                style={{ width: "100%", height: "200px", border: 0 }}
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1561000.1261505196!2d46.435583621308844!3d40.1683748498289!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40307cd91aa21ddf%3A0xe6c9526b3e83cd08!2sAzerbaycan!5e0!3m2!1str!2saz!4v1685464448314!5m2!1str!2saz"
+                style={{ width: "100%", height: "300px", border: 0 }}
                 allowFullScreen=""
                 loading="lazy"
                 referrerPolicy="no-referrer-when-downgrade"
