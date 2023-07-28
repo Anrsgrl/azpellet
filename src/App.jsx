d81ff45d6229dc64f2b23c11ea93ad8f101274fb
Anrsgrl <esgerlianar003@gmail.com>
2023-07-28 08:44:27 +0400

    Refixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,26 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import "./App.scss";
 import 'bootstrap/dist/css/bootstrap.css';
-import error from "./assets/images/error.svg"
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import Location from "./components/Location/Location";
+import GoToTop from "./components/GoToTop/GoToTop";
+import Home from "./pages/Home/Home";
+import About from "./pages/About/About";
+import Contact from "./pages/Contact/Contact";
 function App() {
+  const home = useRef()
+  const about = useRef()
+  const form = useRef()
   return (
     <>
-      {/* <Header home={home} about={about} form={form} />
+      <Header home={home} about={about} form={form} />
       <Home home={home} />
       <About about={about} />
       <Contact form={form} />
       <Location />
       <GoToTop />
-      <Footer /> */}
-      <div className="container d-flex justify-content-center align-items-center flex-column" style={{ height: "100vh" }}>
-        <img src={error} alt="" style={{ width: "80%" }} />
-      </div>
+      <Footer />
     </>
   );
 }
