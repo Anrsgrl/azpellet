3c4d87000f7e7f25883e0a87732ff56074fbc026
Anrsgrl <esgerlianar003@gmail.com>
2024-03-18 21:34:14 +0400

    Update: Copyright year changed

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,32 +1,35 @@
-import React from 'react';
+import React from "react";
 import "./Footer.scss";
 import { FiMail } from "react-icons/fi";
-import { useTranslation } from 'react-i18next';
+import { useTranslation } from "react-i18next";
 
 const Footer = () => {
-    const { t } = useTranslation();
-    return (
-        <footer>
-            <div className="container">
-                <div className="footer-content">
-                    <div className="footer-info pb-2 pb-md-0 flex-column">
-                        <p className='footer-item'>{t('Seller')}</p>
-                        <span className='footer-item'>{t('Copyright')}</span>
-                    </div>
-                    <div className="footer-info flex-column">
-                        <p className='footer-item'>
-                            <FiMail />
-                            <a href="mailto:ttc@azpellet.com">ttc@azpellet.com</a>
-                        </p>
-                        <p className='footer-item'>
-                            <FiMail />
-                            <a href="mailto:office@azpellet.com">office@azpellet.com</a>
-                        </p>
-                    </div>
-                </div>
-            </div>
-        </footer>
-    )
-}
+  const { t } = useTranslation();
+  const today = new Date();
+  return (
+    <footer>
+      <div className="container">
+        <div className="footer-content">
+          <div className="footer-info pb-2 pb-md-0 flex-column">
+            <p className="footer-item">{t("Seller")}</p>
+            <span className="footer-item">
+              {t("Copyright").replace("2023", today.getFullYear())}
+            </span>
+          </div>
+          <div className="footer-info flex-column">
+            <p className="footer-item">
+              <FiMail />
+              <a href="mailto:ttc@azpellet.com">ttc@azpellet.com</a>
+            </p>
+            <p className="footer-item">
+              <FiMail />
+              <a href="mailto:office@azpellet.com">office@azpellet.com</a>
+            </p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
 
 export default Footer;
