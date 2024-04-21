4f798318639ebb3b19d79cb96ad8346ccc7b0d4e
Anrsgrl <esgerlianar003@gmail.com>
2024-04-21 10:03:52 +0400

    Fix: Website fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,26 @@
-import React from "react";
+import React, { useRef } from "react";
 import "./App.scss";
 import "bootstrap/dist/css/bootstrap.css";
-// import Header from "./components/Header/Header";
-// import Footer from "./components/Footer/Footer";
-// import Location from "./components/Location/Location";
-// import GoToTop from "./components/GoToTop/GoToTop";
-// import Home from "./pages/Home/Home";
-// import About from "./pages/About/About";
-// import Contact from "./pages/Contact/Contact";
-import CreditCard from "./pages/About/Components/CreditCard";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import Location from "./components/Location/Location";
+import GoToTop from "./components/GoToTop/GoToTop";
+import Home from "./pages/Home/Home";
+import About from "./pages/About/About";
+import Contact from "./pages/Contact/Contact";
 function App() {
-  // const home = useRef();
-  // const about = useRef();
-  // const form = useRef();
+  const home = useRef();
+  const about = useRef();
+  const form = useRef();
   return (
     <>
-      <CreditCard />
-      {/* <Header home={home} about={about} form={form} />
+      <Header home={home} about={about} form={form} />
       <Home home={home} />
       <About about={about} />
       <Contact form={form} />
       <Location />
       <GoToTop />
-      <Footer /> */}
+      <Footer />
     </>
   );
 }
